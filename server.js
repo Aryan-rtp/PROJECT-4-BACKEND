cc54d54a7f816dc5ac82203071add79c9f802d31
Aryan Pandey <rtp.aryan@gmail.com>
2026-02-08 02:30:59 +0530

    Fix typo in chatHistory variable name

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id)
-    chatHIstory.length = 0
+    chatHistory.length = 0
   })
 
   socket.on("ai-message", async (data) => {
